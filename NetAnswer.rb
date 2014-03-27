3c6827bdf38fe6b7167c2c32848795fcc7e3dc6a
Nacamura <t.nacamura@gmail.com>
2014-03-27 22:40:04 +0900

    Implement logout

--- NetAnswer.rb
+++ NetAnswer.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class NetAnswer
   def call
-	csv = get_detail_csv(get_mechanize_res)
+    csv = get_mechanize_res
     csv.save_as('./detail_' + Time.now.strftime("%Y%m") + '.csv')
   end
 
@@ -15,11 +15,10 @@ class NetAnswer
     login_form = login.form
     login_form.field_with(:name=>'inputId').value = auth['id']
     login_form.field_with(:name=>'inputPassword').value = auth['password']
-    login_form.click_button
-  end
-
-  def get_detail_csv(mechanize_res)
-    detail = mechanize_res.link_with(:text=>'利用明細確認').click
-    csv_file = detail.link_with(:text=>'CSVダウンロード').click
+    mypage = login_form.click_button
+    detail = mypage.link_with(:text=>'利用明細確認').click
+    csv = detail.link_with(:text=>'CSVダウンロード').click
+    mypage.link_with(:text=>'ログアウト').click
+    csv
   end
 end
